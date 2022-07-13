2dca0c0e8ece73a2e8db947a8be952640b40a372
Edgar-Macedo <edgar.ml0814@gmail.com>
2022-07-13 14:27:26 -0500

    Fixed banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
     return (
-      <img src="../src/images/banner.jpg" alt="Rick and Morty" className='banner' />
+      <img src="https://i.pinimg.com/originals/1e/82/7a/1e827ad3bf339cfa3d1528d57c7fc869.jpg" alt="Rick and Morty" className='banner' />
     );
 };
 
